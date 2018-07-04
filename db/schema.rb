e8eb27acfc03f4496ba0b0fce2f6f4ddab5a9f97
iiffd <sml.hng@gmail.com>
2018-07-03 22:26:32 -0700

    Add enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_050132) do
+ActiveRecord::Schema.define(version: 2018_07_04_052131) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_050132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
