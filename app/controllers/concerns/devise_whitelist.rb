7c80407ffcb6b254f5b26ca2c1e8b6edcc46e7c0
iiffd <sml.hng@gmail.com>
2018-07-25 22:25:43 -0700

    Fix glaring bugs

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,8 +1,8 @@
-module DeviseWhiteList
-  extend ActiveSupoort::Concern
+module DeviseWhitelist
+  extend ActiveSupport::Concern
 
   included do
-    before_filter :configure_permitted_parameters, if devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
